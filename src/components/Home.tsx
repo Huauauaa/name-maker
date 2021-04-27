d795a2f5b011eada92274825da69e935532a361c
huaweiwei <harvey0379@163.com>
2021-04-27 13:53:30 +0800

    ✨ two words

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -16,17 +16,25 @@ const { Search } = Input;
 const pinyin = require('pinyin');
 
 function Home() {
-  const cachedCandidates = JSON.parse(
-    localStorage.getItem('candidates') || '[]',
+  const cachedCandidates1 = JSON.parse(
+    localStorage.getItem('candidates1') || '[]',
+  );
+  const cachedCandidates2 = JSON.parse(
+    localStorage.getItem('candidates2') || '[]',
   );
   const [, setInput] = useState('');
   const [target, setTarget] = useState([]);
   const [wordInfo, setWordInfo] = useState([]);
-  const [candidates, setCandidates]: [
+  const [candidates1, setCandidates1]: [
     any,
     Dispatch<SetStateAction<any>>,
-  ] = useState(cachedCandidates);
-  const [candidateInput, setCandidateInput] = useState('');
+  ] = useState(cachedCandidates1);
+  const [candidates2, setCandidates2]: [
+    any,
+    Dispatch<SetStateAction<any>>,
+  ] = useState(cachedCandidates2);
+  const [candidateInput1, setCandidateInput1] = useState('');
+  const [candidateInput2, setCandidateInput2] = useState('');
 
   const onSearch = async (value: string) => {
     setInput(value);
@@ -55,8 +63,12 @@ function Home() {
   };
 
   useEffect(() => {
-    localStorage.setItem('candidates', JSON.stringify(candidates));
-  }, [candidates]);
+    localStorage.setItem('candidates1', JSON.stringify(candidates1));
+  }, [candidates1]);
+
+  useEffect(() => {
+    localStorage.setItem('candidates2', JSON.stringify(candidates2));
+  }, [candidates2]);
 
   const columns = [
     {
@@ -83,19 +95,34 @@ function Home() {
     },
   ];
 
-  const onAddCandidate = (e: SyntheticEvent) => {
-    const newCandidates = candidateInput.split('').map((item: string) => {
-      return {
-        id: uuid(),
-        name: item,
-      };
-    });
-    setCandidates(_.unionBy(candidates, newCandidates, 'name'));
-    setCandidateInput('');
+  const onAddCandidate = (position: string) => (e: SyntheticEvent) => {
+    if (position === '1') {
+      const newCandidates = candidateInput1.split('').map((item: string) => {
+        return {
+          id: uuid(),
+          name: item,
+        };
+      });
+      setCandidates1(_.unionBy(candidates1, newCandidates, 'name'));
+      setCandidateInput1('');
+    } else {
+      const newCandidates = candidateInput2.split('').map((item: string) => {
+        return {
+          id: uuid(),
+          name: item,
+        };
+      });
+      setCandidates2(_.unionBy(candidates2, newCandidates, 'name'));
+      setCandidateInput2('');
+    }
   };
 
-  const onRemoveCandidate = (id: string) => {
-    setCandidates(candidates.filter((item: Candidate) => item.id !== id));
+  const onRemoveCandidate = (id: string, position: string) => {
+    if (position === '1') {
+      setCandidates1(candidates1.filter((item: Candidate) => item.id !== id));
+    } else {
+      setCandidates2(candidates2.filter((item: Candidate) => item.id !== id));
+    }
   };
 
   return (
@@ -112,21 +139,47 @@ function Home() {
         {target.map((item: NameType) => item.name).join(', ')}
       </div>
       <Input
-        addonAfter={<PlusOutlined onClick={onAddCandidate} />}
-        value={candidateInput}
+        addonAfter={<PlusOutlined onClick={onAddCandidate('1')} />}
+        value={candidateInput1}
         allowClear
-        onPressEnter={onAddCandidate}
+        onPressEnter={onAddCandidate('1')}
         onChange={(e) => {
-          setCandidateInput(e.target.value.replace(' ', ''));
+          setCandidateInput1(e.target.value.replace(' ', ''));
         }}
+        placeholder="请输入第一个字"
       />
       <div className="candidates">
-        {candidates.map((item: Candidate) => {
+        {candidates1.map((item: Candidate) => {
           return (
             <Tag
               key={item.id}
               closable
-              onClose={() => onRemoveCandidate(item.id)}
+              onClose={() => onRemoveCandidate(item.id, '1')}
+              className="candidate-item"
+            >
+              {item.name}
+            </Tag>
+          );
+        })}
+      </div>
+
+      <Input
+        addonAfter={<PlusOutlined onClick={onAddCandidate('2')} />}
+        value={candidateInput2}
+        allowClear
+        onPressEnter={onAddCandidate('2')}
+        onChange={(e) => {
+          setCandidateInput2(e.target.value.replace(' ', ''));
+        }}
+        placeholder="请输入第二个字"
+      />
+      <div className="candidates">
+        {candidates2.map((item: Candidate) => {
+          return (
+            <Tag
+              key={item.id}
+              closable
+              onClose={() => onRemoveCandidate(item.id, '2')}
               className="candidate-item"
             >
               {item.name}
